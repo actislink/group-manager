0c8b58ec009cf98e9b19770258637a05638a45fa
Wojciech Wątroba <wojciech.watroba@unicosoftware.com>
2015-10-06 12:55:59 +0200

    merge

--- src/main/java/com/actislink/rest/GroupController.java
+++ src/main/java/com/actislink/rest/GroupController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
@@ -12,16 +13,16 @@ import javax.ws.rs.core.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.actislink.bussines.GroupManager;
 import com.actislink.dao.AlreadyExistException;
 import com.actislink.model.GroupCreation;
 import com.actislink.model.GroupId;
 import com.actislink.model.UserId;
+import com.actislink.service.GroupService;
 
 @Path("/group")
 public class GroupController {
     @Inject
-    private GroupManager groupManager;
+    private GroupService groupService;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GroupController.class);
 
@@ -30,19 +31,31 @@ public class GroupController {
         GroupCreation groupCreation = new GroupCreation(name);
 
         try {
-            groupManager.createGroup(groupCreation);
+            groupService.createGroup(groupCreation);
 
             HttpSession session = req.getSession(false);
 
             GroupId groupId = new GroupId(groupCreation.getName());
             UserId userId = new UserId(session.getAttribute("username").toString());
 
-            groupManager.addUser(groupId, userId);
+            groupService.manage(groupId).addUser(userId);
 
             return Response.status(200).entity("ok").build();
         } catch (AlreadyExistException e) {
             LOGGER.warn("", e);
             return Response.status(500).entity(e.getMessage()).build();
         }
+    }
+
+    @GET
+    @Path("join/{group}")
+    public Response join(@PathParam("group") String group) {
+        return Response.status(200).build();
+    }
+
+    @GET
+    @Path("leave/{group}")
+    public Response leave(@PathParam("group") String group) {
+        return Response.status(200).build();
     }
 }
